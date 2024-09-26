725b0d905cb14e546e97d715117cb122bd469d68
Yao-JX <D1345310@o365.fcu.edu.tw>
2024-09-26 10:43:01 +0800

    '0926-5'

--- t205.py
+++ t205.py
@@ -2,6 +2,6 @@ n=str(input('輸入三位數：'))
 if int(n)<999:
     y=[int(x) for x in n]
     y[0],y[2]=y[2],y[0]
-    print('結果：',''.join(map(str,y)))
+    print('結果：',''.join(map(str,y)))#print(y[0],y[1],y[2],y[3])會有空格
 else:
     print("error")
