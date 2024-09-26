3fa46fdc288f3ebcb46d9b082f197639dabf3ae7
Yao-JX <D1345310@o365.fcu.edu.tw>
2024-09-26 20:14:43 +0800

    '0926-1'

--- t201.py
+++ t201.py
@@ -1,6 +1,6 @@
 a=int(input('輸入變數a:'))
 b=int(input('輸入變數b:'))
 a,b=b,a
-print('交換後的結果:')
-print('a=',a,sep='') #sep=''是為了不要印出空格
-print('b=',b,sep='')
+print('交換後的結果:\n','a=',a,'\n','b=',b,sep='')
+# print('a=',a,sep='') #sep=''是為了不要印出空格
+# print('b=',b,sep='')
